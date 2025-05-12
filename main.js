50679912cd6f3559cf5c6bcdac573416a50d7159
Betina9 <ob042025@gmail.com>
2025-05-12 11:14:55 +0200

    trying out theme mode

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const toggleButton = document.getElementById("toggle-btn");
-function themeSwitcher() {
-  document.body.classList.toggle("dark mode");
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark mode");
 }
+if (document.body.classList.contains("dark-mode")) {
+    btn.textContent = "Swith to light mode";
+} else {
+    btn.textContent =Switch to dark mode";
+}
+});
