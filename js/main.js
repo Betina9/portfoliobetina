633b8bb2b092ba1e7081339e72d7e9f3c9b62e3c
Betina9 <ob042025@gmail.com>
2025-05-12 12:11:39 +0200

    trying 2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("toggle-theme");
-  button.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
+const button = document.getElementById("toggle-btn");
 
-    if (document.body.classList.contains("dark-mode")) {
-      button.textContent = "Bytt til light mode";
-    } else {
-      button.textContent = "Bytt til dark mode";
-    }
-  });
+button.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+
+  button.textContent = document.body.classList.contains("dark-mode")
+    ? "Bytt til light mode"
+    : "Bytt til dark mode";
 });
